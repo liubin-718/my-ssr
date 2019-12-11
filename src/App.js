9ffa707d1799beb5e6101c528bbb933e2429c631
vue <51242759+liubin-718@users.noreply.github.com>
2019-12-11 14:45:15 +0800

    Update App.js
    更改

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react'
-function App(){
-    return <h1>开课吧</h1>
+import React, {useState} from 'react'
+function App(props){
+    const [count, setCount] = useState(1)
+    return <>
+                <h1>我的第一个react ssr, {props.title}</h1>
+                <p>如果你喜欢，请点赞，当前已经获得star： {count} 🌟</p>
+                <button onClick={()=>setCount(count+1)}>点赞👍</button>
+           </>
 } 
-export default App;
+export default <App title="kkb"><App>;
